a418736742f948d27e65750586881e606e3b9e7e
Hannah Wayment-Steele <hannahw1@stanford.edu>
2021-09-20 07:54:24 -0400

    update calculate Zscore demo

--- DEMO/calculateZscoreDEMO.py
+++ DEMO/calculateZscoreDEMO.py
@@ -11,13 +11,16 @@ RS_files = ['RS_pearson_Dataset_%s_bps_BOOTSTRAPS.json.zip' % x for x in package
 CM = pd.DataFrame()
 for fil in CM_files:
 	CM = CM.append(pd.read_json(fil), ignore_index=True)
-
+print('Chem Mapping Rnd 1 scores')
 cm_zscores, _ = calculate_Z_scores(CM)
 print(cm_zscores[['package','pearson_mean','pearson_std','pearson_zscore_by_Dataset_mean']])
 
 RS = pd.DataFrame()
 for fil in RS_files:
 	RS = RS.append(pd.read_json(fil), ignore_index=True)
+
+print('')
+print('Riboswitch "Ribologic FMN" scores')
 
 rs_zscores, _ = calculate_Z_scores(RS)
 print(rs_zscores[['package','pearson_mean','pearson_std','pearson_zscore_by_Dataset_mean']])
